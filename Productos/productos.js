5e21ee1774289560858c03f7e9517bc9e79f4bf5
Daniel <moreno.danielr@hotmail.com>
2021-10-12 12:15:39 -0500

    Modulo de prodcutos

--- Productos/productos.js
+++ Productos/productos.js
@@ -1,6 +1,3 @@
-
-// Firebase
-
 const firebaseConfig = {
     apiKey: "AIzaSyDCAa6J48jm7cEHdKJJt2Qzbu6jKVUg-Hc",
     authDomain: "driverspub.firebaseapp.com",
@@ -10,13 +7,21 @@ const firebaseConfig = {
     appId: "1:716939539079:web:17d7255d86d33de5a1be22"
 };
 
+
+
+
 // Inicializacion db
 
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 // variables
 
 const database = firebase.firestore()
+const dataBase = firebaseApp.firestore();
+const onProducts = (callback) => dataBase.collection("productos").onSnapshot(callback);
+productList();
+
+
 
 // Variables DOM
 
@@ -25,6 +30,61 @@ const descripcion = document.getElementById('descripcion')
 const valorUnitario = document.getElementById('valor-unitario')
 const estado = document.getElementById('estadoProducto')
 const formularioModal = document.getElementById('formulario-registro-producto')
+
+
+
+
+
+//UserList gets the users collection in real time, this function list users and its modifies atributes into the table body.
+async function productList() {
+
+    onProducts((prodcuts) => {
+  
+      const table = document.getElementById("tableBody");
+      table.innerHTML = '';
+  
+      prodcuts.forEach(product => {
+        const prodcutData = product.data();
+        let newRow = table.insertRow();
+        let newCell = newRow.insertCell(-1);
+        let newCell2 = newRow.insertCell(-1);
+        let newCell3 = newRow.insertCell(-1);
+        let newCell4 = newRow.insertCell(-1);
+        let newCell5 = newRow.insertCell(-1);
+        let newText = document.createTextNode(product.id);
+        let newText2 = document.createTextNode(prodcutData.descripcion);
+        let newText3 = document.createTextNode(prodcutData.valorUnitario);
+        let newText4 = document.createTextNode(prodcutData.estado);
+        const button = document.createElement("button");
+        button.type = "button";
+        button.innerHTML = ('<i class="fas fa-pen-square"></i>');
+        button.className = "btn btn-dark";
+        button.dataset.bsToggle = "modal";
+        button.dataset.bsTarget = "#exampleModal"
+        button.dataset.user = prodcutData.nombre;
+        button.dataset.bsWhatever = prodcutData.correo;
+        newCell.appendChild(newText);
+        newCell2.appendChild(newText2);
+        newCell3.appendChild(newText3);
+        newCell4.appendChild(newText4);
+        newCell5.appendChild(button);
+  
+      });
+  
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Funciones
 
