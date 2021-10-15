e094ebb4ec4892e11255f55eac21eba1d2d829dd
arielramosc <90631922+arielramosc@users.noreply.github.com>
2021-10-15 10:01:47 -0500

    Create modalProduct.js
    crear modal para lista de productos

---
+++ Productos/modalProduct.js
@@ -0,0 +1,20 @@
+// export const indexbutto = document.getElementById('exampleModal').getAttribute.button;
+// console.log(indexbutto)
+const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    // console.log(button);
+    // Extract info from data-bs-* attributes
+    
+    const recipient = button.getAttribute('data-bs-whatever');
+    const producto = button.getAttribute('data-user'); // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    let modalTitle = exampleModal.querySelector('.modal-title')
+    let modalBodyInput = exampleModal.querySelector('.modal-body input')
+    // console.log(usuario,recipient);
+    modalTitle.textContent = 'Actualizar Producto ' + producto
+    modalBodyInput.value = recipient
+});
