bbdd2bcaece49fa812736f1ee63b9523ecda896c
arielramosc <90631922+arielramosc@users.noreply.github.com>
2021-10-19 16:04:39 -0500

    Se activa barra de búsqueda

---
+++ Productos/buscar.js
@@ -0,0 +1,44 @@
+
+
+  
+  const LightTableFilter = (function(Arr) {
+
+    let _input;
+
+    function _onInputEvent(e) {
+      _input = e.target;
+      const tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+      Arr.forEach.call(tables, function(table) {
+        Arr.forEach.call(table.tBodies, function(tbody) {
+          Arr.forEach.call(tbody.rows, _filter);
+        });
+      });
+    }
+
+    function _filter(row) {
+      const text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+      row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+    }
+
+    return {
+      init: function() {
+        const inputs = document.getElementsByClassName('light-table-filter');
+        Arr.forEach.call(inputs, function(input) {
+          input.oninput = _onInputEvent;
+        });
+      }
+    };
+  })(Array.prototype);
+
+  document.addEventListener('readystatechange', function() {
+    if (document.readyState === 'complete') {
+      LightTableFilter.init();
+    }
+  });
+
+
+
+
+
+
+  
